dde38bf34a807d80509361a8f6b486b599d7aeb0
shown <133968357+shownnn@users.noreply.github.com>
2025-05-17 10:49:21 -0400

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,12 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Set-DisplayResolution -Width 1920 -Height 1080 -Force
+
+$crdPath = "$env:TEMP\chromeremotedesktophost.msi"
+Start-BitsTransfer -Source "https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi" -Destination $crdPath
+Start-Process "msiexec.exe" -ArgumentList "/i `"$crdPath`" /qn" -Wait
+Remove-Item $crdPath
+
+$chromePath = "$env:TEMP\chrome_installer.exe"
+Start-BitsTransfer -Source "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -Destination $chromePath
+Start-Process -FilePath $chromePath -ArgumentList "/install", "/silent", "/system-level" -Verb RunAs -Wait
+Remove-Item $chromePath
