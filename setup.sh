529febf74f257dff33d2e7d13500542bf98ec59f
shown <133968357+shownnn@users.noreply.github.com>
2025-05-18 15:50:56 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+sudo ufw disable
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y wget curl gdebi-core desktop-base xscreensaver
+
+sudo apt install -y xfce4
+
+echo "exec /usr/bin/xfce4-session" > ~/.chrome-remote-desktop-session
+
+wget -O /tmp/chrome-remote-desktop.deb https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo gdebi -n /tmp/chrome-remote-desktop.deb
+rm /tmp/chrome-remote-desktop.deb
+
+wget -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi -n /tmp/google-chrome.deb
+rm /tmp/google-chrome.deb
